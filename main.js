a2a810b0457c1b56ef4e1f088d98115902b3bf58
elizarov017 <ftuti60@gmail.com>
2021-02-04 03:26:04 +0200

    0.03b
    added animation of adding and deleting of element

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 $(function() {
+    let LIST = $(".itemList");
+    let itemTemplate = $(".row").html();
+
     $(window).load(function() {
         addItem("Огірки");
         addItem("Помідори");
@@ -10,21 +13,33 @@ $(function() {
         if (title) addItem(title);
         $(".area").val("");
     });
-    let LIST = $(".itemList");
-let itemTemplate = $(".row").html();
-
 
 function addItem(title)	{
     let node = $(itemTemplate);
     node.find(".bl-product").text(title);	
 
     node.find(".delete").click(function(){
-    node.remove();
+        // disappear(node);
+        node.slideUp(500, function() {
+            node.remove();
+        });
+        
+    });
+    
+    LIST.append(node);
+    
+    // node.addClass("ovr-hid");
+
+    node.animate({
+            height: "65px"
     });
 
-    LIST.append(node);
-    };
 
-});
+};})
 
+function disappear(node) {
+    node.animate({
+        height: "0px"
+    });
+}
 
