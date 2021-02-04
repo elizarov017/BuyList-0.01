c316a2bb93fb20140577c1c6e1cc12cda29f9d70
elizarov017 <ftuti60@gmail.com>
2021-02-05 00:23:21 +0200

    0.03c

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $(function() {
-    let LIST = $(".itemList");
+    let itemList = $(".itemList");
     let itemTemplate = $(".row").html();
+    let badgeList = $(".buy-list");
+    let badgeTemplate = $(".one-badge").html();
+
 
     $(window).load(function() {
         addItem("Огірки");
@@ -13,33 +16,47 @@ $(function() {
         if (title) addItem(title);
         $(".area").val("");
     });
+    
 
 function addItem(title)	{
     let node = $(itemTemplate);
+    let badge = $(badgeTemplate);
+    let count = 1;
+
     node.find(".bl-product").text(title);	
+    badge.find(".b-name").text(title);
+    node.find(".bl-label").text(count);
+    badge.find(".labela").text(count);
 
     node.find(".delete").click(function(){
-        // disappear(node);
         node.slideUp(500, function() {
             node.remove();
         });
-        
+        badge.remove();
     });
-    
-    LIST.append(node);
-    
-    // node.addClass("ovr-hid");
 
+    node.find(".bl-minus").click(function() {
+        node.find(".bl-label").text(--count);
+        badge.find(".labela").text(count);
+        if (count == 1) node.find(".bl-minus").attr('disabled', 'disabled');
+    });
+
+    node.find(".bl-plus").click(function() {
+        node.find(".bl-label").text(++count);
+        badge.find(".labela").text(count);
+        if (count > 1) node.find(".bl-minus").removeAttr('disabled');
+    });
+
+
+
+
+
+    itemList.append(node);
+    badgeList.append(badge);
     node.animate({
             height: "65px"
     });
-
-
 };})
 
-function disappear(node) {
-    node.animate({
-        height: "0px"
-    });
-}
+
 
