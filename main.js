6d40c1e3e2e45610b620d34a5e44c49ccf662b7c
elizarov017 <ftuti60@gmail.com>
2021-02-05 03:38:42 +0200

    0.03d
    pre-final
    just adaptivity went out :)))

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ $(function() {
     let itemList = $(".itemList");
     let itemTemplate = $(".row").html();
     let badgeList = $(".buy-list");
+    let boughtList = $(".bought-list");
     let badgeTemplate = $(".one-badge").html();
 
 
@@ -14,19 +15,35 @@ $(function() {
     $(".add").click(function() {
         let title = $(".area").val();
         if (title) addItem(title);
-        $(".area").val("");
+        $(".area").val("").focus();
+    });
+
+    $(".area").keydown(function(e) {
+        if(e.keyCode === 13) {
+            let title = $(".area").val();
+            if (title) addItem(title);
+            $(".area").val("");
+        }
     });
     
 
 function addItem(title)	{
     let node = $(itemTemplate);
     let badge = $(badgeTemplate);
+    let nBadge = $(badgeTemplate);
     let count = 1;
+
+    nBadge.addClass("hidden").find(".b-name").addClass("line-through");
+    nBadge.find(".labela").addClass("line-through");
 
     node.find(".bl-product").text(title);	
     badge.find(".b-name").text(title);
+    nBadge.find(".b-name").text(title);
+    // node.find(".ch-name").val(title);
+
     node.find(".bl-label").text(count);
     badge.find(".labela").text(count);
+    nBadge.find(".labela").text(count);
 
     node.find(".delete").click(function(){
         node.slideUp(500, function() {
@@ -36,23 +53,73 @@ function addItem(title)	{
     });
 
     node.find(".bl-minus").click(function() {
-        node.find(".bl-label").text(--count);
-        badge.find(".labela").text(count);
+        --count
         if (count == 1) node.find(".bl-minus").attr('disabled', 'disabled');
+        node.find(".bl-label").text(count);
+        badge.find(".labela").text(count);
+        nBadge.find(".labela").text(count);
     });
 
     node.find(".bl-plus").click(function() {
-        node.find(".bl-label").text(++count);
-        badge.find(".labela").text(count);
+        count++;
         if (count > 1) node.find(".bl-minus").removeAttr('disabled');
+        node.find(".bl-label").text(count);
+        badge.find(".labela").text(count);
+        nBadge.find(".labela").text(count);
+        
     });
 
+    node.find(".bought").click(function() {
+        node.fadeOut(250, function() {
+        node.find(".bought").addClass("hidden");
+        node.find(".delete").addClass("hidden");
+        node.find(".n-bought").removeClass("hidden");
+        node.find(".bl-plus").addClass("hidden");
+        node.find(".bl-minus").addClass("hidden");
 
+        node.find(".bl-product").addClass("line-through");
+        badge.addClass("hidden");
+        nBadge.removeClass("hidden");
+        node.fadeIn(250);
+        });
+    })
 
+    node.find(".n-bought").click(function() {
+        node.fadeOut(250, function() {
+            node.find(".bought").removeClass("hidden");
+            node.find(".delete").removeClass("hidden");
+            node.find(".n-bought").addClass("hidden");
+            node.find(".bl-plus").removeClass("hidden");
+            node.find(".bl-minus").removeClass("hidden");
+            node.fadeIn(250);
+        });
+        
 
+        node.find(".bl-product").removeClass("line-through");
+        badge.removeClass("hidden");
+        nBadge.addClass("hidden");
+    })
+
+    node.find(".bl-product").click(function() {
+        if (!node.find(".bl-product").hasClass("line-through")) {
+        node.find(".bl-product").addClass("hidden");
+        node.find(".ch-name").val(node.find(".bl-product").text())
+        node.find(".ch-name").removeClass("hidden");
+        node.find(".ch-name").focus();
+        }
+    });
+
+    node.find(".ch-name").blur(function() {
+        node.find(".bl-product").text(node.find(".ch-name").val());
+        badge.find(".b-name").text(node.find(".ch-name").val());
+        nBadge.find(".b-name").text(node.find(".ch-name").val());
+        node.find(".bl-product").removeClass("hidden");
+        node.find(".ch-name").addClass("hidden");
+    });
 
     itemList.append(node);
     badgeList.append(badge);
+    boughtList.append(nBadge);
     node.animate({
             height: "65px"
     });
